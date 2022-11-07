b89bdb58a72bd81707a755f9904d1de702f18738
SamitMahadi <samithossainmahadi@gmail.com>
2022-11-07 15:12:04 -0500

    carusel style done

--- src/Pages/Home/Carusel/Carusel.js
+++ src/Pages/Home/Carusel/Carusel.js
@@ -8,35 +8,47 @@ import img4 from '../../../Assets/carusel img/pexels-kampus-production-8430307.j
 const Carusel = () => {
     return (
         <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src={img1} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle bg-red-700">❮</a> 
-            <a href="#slide2" className="btn btn-circle bg-red-700">❯</a>
-          </div>
-        </div> 
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src={img2} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle bg-red-700">❮</a> 
-            <a href="#slide3" className="btn btn-circle bg-red-700">❯</a>
-          </div>
-        </div> 
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src={img3} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle bg-red-700">❮</a> 
-            <a href="#slide4" className="btn btn-circle bg-red-700">❯</a>
-          </div>
-        </div> 
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src={img4} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle bg-red-700">❮</a> 
-            <a href="#slide1" className="btn btn-circle bg-red-700">❯</a>
-          </div>
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={img1} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2  px-40 top-1/4">
+                    <h1 className='text-6xl font-bold text-white'>Transformation Through Consulting</h1>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2  px-40 top-1/2 text-white">
+                    <p>The Process Consultant is no longer consulting. I want to thank my many clients for placing their trust and patience in me over the years. If you are looking for professional business process consulting, I recommend you contact the excellent team at Business Enterprise Mapping who were my serious competition and whose work I always admired.</p>
+
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2  top-3/4 left-1/2 text-white">
+                    <button class="bg-red-700 hover:bg-red-800 text-white font-bold py-4 px-10 rounded">
+                        Get More
+                    </button>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle bg-red-700">❮</a>
+                    <a href="#slide2" className="btn btn-circle bg-red-700">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={img2} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle bg-red-700">❮</a>
+                    <a href="#slide3" className="btn btn-circle bg-red-700">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={img3} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle bg-red-700">❮</a>
+                    <a href="#slide4" className="btn btn-circle bg-red-700">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={img4} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle bg-red-700">❮</a>
+                    <a href="#slide1" className="btn btn-circle bg-red-700">❯</a>
+                </div>
+            </div>
         </div>
-      </div>
     );
 };
 
