ffc343ef196bc3e76fea1bb6b7c2a60f933e2c5b
SamitMahadi <samithossainmahadi@gmail.com>
2022-11-07 12:08:42 -0500

    first commit

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Main from '../../Layout/Main';
+import Home from '../../Pages/Home/Home/Home';
+const { createBrowserRouter } = require("react-router-dom");
+const router =createBrowserRouter([
+    {
+        path:'/',
+        element: <Main></Main>,
+        children:[
+            {
+                path:'/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+export default router;
