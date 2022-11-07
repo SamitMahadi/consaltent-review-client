b695064fdfa85436c23c20cceb563e279204fb89
SamitMahadi <samithossainmahadi@gmail.com>
2022-11-07 12:35:59 -0500

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import whiteLogo from '../../../Assets/logo/white-logo.png'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>this is footer</h1>
-        </div>
+        <footer className="footer p-10 bg-blue-800 text-white">
+            <div>
+                <img src={whiteLogo} alt="" />
+                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+            </div>
+            <div>
+                <span className="font-bold text-white text-2xl">Services</span>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="font-bold text-white text-2xl">Company</span>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="font-bold text-white text-2xl">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
