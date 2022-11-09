f33e36aa7cfdc6c2123b0420257f89f02d2e8ffc
SamitMahadi <samithossainmahadi@gmail.com>
2022-11-09 11:19:59 -0500

    serviesdetails added

--- src/Pages/Services/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
-                
-    const {title} =useLoaderData();
-    
-   
+
+    const { title, img, discription } = useLoaderData();
+
+
     return (
-        <div>
-            <h1 className='text-3xl'>{title}</h1>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={img} className="w-1/2 h-full rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">{title}</h1>
+                    <h2 className="py-6 text-lg ">{discription}</h2>
+                    
+                </div>
+            </div>
         </div>
     );
 };
