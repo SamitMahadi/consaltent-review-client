b71600581145afbccdb3ef7705aa536e3815f62b
SamitMahadi <samithossainmahadi@gmail.com>
2022-12-21 11:16:59 -0500

    commit

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,13 +6,6 @@ import logo from '../../../Assets/logo/logo.png'
 import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
-
-    const menuItems = <>
-        <Link className='text-black decoration-none text-xl font-semibold px-5 ml-4' to='/'>Home</Link>
-        <Link className='text-black decoration-none text-xl font-semibold px-5' to='/services'>Services</Link>
-        <Link className='text-black decoration-none text-xl font-semibold px-5' to='/blogs'>Blog</Link>
-    </>
-
     const { user,logOut } = useContext(AuthContext);
 
     const handleLogOut= ()=>{
@@ -21,29 +14,11 @@ const Header = () => {
         .catch(error=>console.error(error))
     }
 
-    return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-center">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {menuItems}
-                    </ul>
-                </div>
-                <Link>
-                    <img src={logo} alt="" />
-                </Link>
-            </div>
-            <div className="navbar-start hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    {menuItems}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <h1 className='text-xl mr-4'>
-                    {
+    const menuItems = <>
+        <Link className='text-black decoration-none text-xl font-semibold px-5 ml-4' to='/'>Home</Link>
+        <Link className='text-black decoration-none text-xl font-semibold px-5' to='/services'>Services</Link>
+        <Link className='text-black decoration-none text-xl font-semibold px-5' to='/blogs'>Blog</Link>
+        {
                         user?.uid?
                         <>
                         <Link className='text-black decoration-none text-xl font-semibold px-5' to='/myReview'>Reviews</Link>
@@ -58,6 +33,33 @@ const Header = () => {
                         <Link className='text-black decoration-none text-xl font-semibold px-5' to='/signup'>Signup</Link>
                         </>
                     }
+    </>
+
+  
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-center">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    {menuItems}
+                    </ul>
+                </div>
+                <Link>
+                    <img src={logo} alt="" />
+                </Link>
+            </div>
+            <div className="navbar-start hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <h1 className='text-xl mr-4'>
+                    
                 </h1>
                 <p className="">{user?.photoURL?
                  
